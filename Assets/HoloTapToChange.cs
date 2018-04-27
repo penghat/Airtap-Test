a055822a364150b8a0e61b9078befd9fffbafc50
Ananth Penghat <apenghat@wireless-10-104-237-138.umd.edu>
2018-04-27 16:30:16 -0400

    Update airtap example

---
+++ Assets/HoloTapToChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class HoloTapToChange : MonoBehaviour, IInputClickHandler, IFocusable {
+
+	public void OnInputClicked(InputClickedEventData eventData) {
+		Debug.Log ("Clicked!");
+		GetComponent<Renderer>().material.color = Random.ColorHSV();
+	}
+
+	public void OnFocusEnter() {
+		transform.localScale *= 1.2f; // enlarge cube while gazed at
+	}
+	public void OnFocusExit() {
+		transform.localScale /= 1.2f; // shrink cube while gazed at
+	}
+
+}
---
+++ Assets/TapColorChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class TapColorChange : MonoBehaviour {
+
+	GestureRecognizer recognizer;
+
+	void Awake() {
+		recognizer = new GestureRecognizer();
+		recognizer.SetRecognizableGestures(GestureSettings.Tap);
+		recognizer.StartCapturingGestures();
+	}
+
+	void OnEnable() {
+		recognizer.TappedEvent += Recognizer_TappedEvent;
+	}
+
+	void OnDisable() {
+		recognizer.TappedEvent -= Recognizer_TappedEvent;
+	}
+
+	void Update () {
+		// simulate tap with mouse button: NOT NECESSARY FOR HOLOLENS, COULD DELETE ENTIRE METHOD
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("it's click");
+			Recognizer_TappedEvent(
+				InteractionSourceKind.Other, 1, Camera.main.ScreenPointToRay(Input.mousePosition));
+		}
+	}
+
+	private void Recognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay) {
+		GetComponent<Renderer>().material.color = Random.ColorHSV();
+	}
+}
